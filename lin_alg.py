58bdf8b323423b7c9f7c79bf3059121a5beca9fc
saadnasirajk5-tech <saadnasirajk5@gmail.com>
2025-11-17 10:28:43 -0800

    first commit

---
+++ lin_alg.py
@@ -0,0 +1,14 @@
+# dot product 
+import numpy as np 
+v1 = np.array([1,2,3]) 
+v2 = np.array([3,4,5]) 
+dot_product = np.dot(v1,v2) 
+print(dot_product)      
+
+# writing a polynomial with numpy  
+poly_1 = np.poly1d([3,2,1]) 
+print(poly_1(2)) 
+
+
+
+
